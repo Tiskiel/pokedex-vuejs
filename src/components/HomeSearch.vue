08f7578d9909173027e847ce7389b0c4c07efbc0
Wanty Jean-François <wanty.jf@gmail.com>
2024-10-20 20:32:03 +0200

    Add the input to search a pokemon

---
+++ src/components/HomeSearch.vue
@@ -0,0 +1,42 @@
+<script>
+    export default {
+        data() {
+            return {
+                search: ''
+            }
+        },
+        methods: {
+            searchPokemon() {
+                console.log("Searching for pokemon: ", this.search);
+            }
+        }
+    }
+</script>
+
+<template>
+  <div class="home-search">
+    <input
+      type="text"
+      placeholder="Search for a pokemon"
+      class="search"
+      v-model="search"
+      @input="searchPokemon"
+    />
+  </div>
+</template>
+
+<style>
+.home-search {
+  display: flex;
+  justify-content: center;
+  margin-top: 2rem;
+
+    .search {
+        padding: 0.6rem 1rem;
+        border: 1px solid green;
+        border-radius: 8px;
+        width: 100%;
+        max-width: 400px;
+    }
+}
+</style>
